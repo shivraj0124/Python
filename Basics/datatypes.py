b475010eea9c4709ec986bd992ab88c456cfb9ad
shivraj0124 <120171741+shivraj0124@users.noreply.github.com>
2022-12-09 15:24:36 +0530

    Add files via upload

---
+++ Basics/datatypes.py
@@ -0,0 +1,42 @@
+a = " Python"
+print("Data Type of ",a," : ",type(a),)
+
+a = 10
+print("Data Type of ",a," : ",type(a),)
+
+a = 10.22
+print("Data Type of ",a," : ",type(a),)
+
+a = 1j
+print("Data Type of ",a," : ",type(a),)
+
+a = ["Python","Javascript","Java","C++"]
+print("Data Type of ",a," : ",type(a),)
+
+
+a = ("Python","Javascript","Java","C++")
+print("Data Type of ",a," : ",type(a),)
+
+# a = range(b)
+# print("Data Type of ",a," : ",type(a),)
+
+a = {"Python","Javascript","Java","C++"}
+print("Data Type of ",a," : ",type(a),)
+
+a = True
+print("Data Type of ",a," : ",type(a),)
+
+a = b"pyt"
+print("Data Type of ",a," : ",type(a),)
+
+a = bytearray(6)
+print("Data Type of ",a," : ",type(a),)
+
+a = frozenset({"Python","Javascript","Java","C++"})
+print("Data Type of ",a," : ",type(a),)
+
+a = memoryview(bytes(6))
+print("Data Type of ",a," : ",type(a),)
+
+a=None
+print("Data Type of ",a," : ",type(a),)
