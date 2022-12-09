17133d7338f76a68e4d8fa89e564eb2ea63f5a82
shivraj0124 <120171741+shivraj0124@users.noreply.github.com>
2022-12-09 15:54:27 +0530

    Strings

--- Basics/Strings.py
+++ Basics/Strings.py
@@ -1,4 +1,19 @@
-Firstname ="Raj "
-Lastname ="Sharma "
-
+# Strings with Single quote (Single inverted commas)
+Firstname ='Sahil '
+Lastname ='Kumar'
 print(Firstname + Lastname)
+
+# Strings with Double quotes (Double inverted commas)
+Firstname ="Raj "
+Lastname ="Sharma"
+print(Firstname + Lastname)
+
+# Strings with 3-Single quote (Single inverted commas) Multiline string
+info = ''' Learn 
+Python from 
+basics '''
+
+# Strings with 3-Double quotes (Double inverted commas) Multiline string
+info = """ Learn 
+Python from 
+basics """
