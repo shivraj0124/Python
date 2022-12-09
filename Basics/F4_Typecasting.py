8893679a3d9344802f1b337ceb00589b8b50b6ba
shivraj0124 <120171741+shivraj0124@users.noreply.github.com>
2022-12-09 17:25:55 +0530

    Type Casting

---
+++ Basics/F4_Typecasting.py
@@ -0,0 +1,9 @@
+a = "3" # datatype of variable a is string here bcz we wrap 3 in " " 
+b = "5" # datatype of variable b is string here bcz we wrap 5 in " "
+
+print(a + b) # here ouput will be as 35
+
+# We want output as 3+5 =8 
+# So we need to change the data type of a and b
+print(int(a) + int(b)) 
+
--- Basics/typecasting.py
+++
@@ -1,3 +0,0 @@
-a = "3"
-b = "5"
-print(int(a) + int(b))
