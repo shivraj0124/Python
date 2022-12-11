a1826e01a9979bf069894f29dc70588774dcfcbf
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-11 19:59:49 +0530

    Comments
    When you comment any statement in the code, that statement will not execute...

---
+++ Basics/F6_Comments.py
@@ -0,0 +1,10 @@
+print("Learn Python")
+
+# print("Hello World")
+
+# --> # is used for comment in Python
+# Commented part of code will not be execute
+
+# Output: Learn Python
+
+# in this code Hello World will not print because we commented that statement
