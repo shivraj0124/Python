91ebcf268a020d06c879db2629fb79aa2cfe490f
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-11 20:10:52 +0530

    Slicing Strings
    We can print the each character of string using it index...

---
+++ Basics/Strings/F2_SlicingStrings.py
@@ -0,0 +1,10 @@
+b = "Python Javascript!"
+print(b[0:6]) # prints character of string using string index
+# 0 is index of 'P'
+# 5 is index of 'n' 
+# output: Learn
+
+print(b[-11:-1])  
+# -11 is index of 'J'
+# -1  is index of 't'
+# output: Python
