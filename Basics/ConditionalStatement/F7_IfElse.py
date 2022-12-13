ea4f1ac88504dec07390f8b30a48a1a79972945c
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-13 20:44:03 +0530

    IfElse
    An "if statement" is written by using the if keyword where
    The else keyword catches anything which isn't caught by the preceding conditions.

---
+++ Basics/ConditionalStatement/F7_IfElse.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a number: "))
+print("Entered number is:", a)
+
+# Conditional operators 
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+# if condition will true then if block will execute, Output will 'Yes' 
+if(a > 20):
+  print("Yes")
+else:
+  print("No")
+
+# if a > 20 condition get false then else block will execute, Output will 'No'
