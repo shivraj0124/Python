3beec4abfa12138b040b72a419b07b941d929c02
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-15 20:20:51 +0530

    Take_Input
    To take inputs from user we use input() method , it makes program dynamic.

---
+++ Basics/F2_a_Take_Input.py
@@ -0,0 +1,14 @@
+# way1
+name = input("Enter Your name:")
+print(name)
+
+# way 2
+print("Enter your name:")
+name = input()
+print("Hi",name)
+
+# way 3
+number = int(input("Enter your Age:"))
+print(number)
+
+# input() is used to take input from user
