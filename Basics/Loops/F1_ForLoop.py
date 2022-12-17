db0a90e7accc5d4cb6f1084b5dac4848ef9ce7a9
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-17 17:25:10 +0530

    for loop

---
+++ Basics/Loops/F1_ForLoop.py
@@ -0,0 +1,7 @@
+# for loop is used for reapeating statements , also use for iterating a string,list, etc
+
+name = "Google" 
+for i in name:
+    print(i)
+    
+ # prints each character present in name variable
