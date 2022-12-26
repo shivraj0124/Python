5d815dcced3d21670d5466d7fbee77d7911b1ec6
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-26 21:07:22 +0530

    List

---
+++ Basics/List/F1_List.py
@@ -0,0 +1,6 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# List is one of the built in data type in python
+# They are used store different and same type of data items
+# It can be modify or change
