e1a602c3dd0ac84be9ecd0dbb119a02fd5b99b0b
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-26 22:29:01 +0530

    Add Element

---
+++ Basics/List/F4_AddELment.py
@@ -0,0 +1,12 @@
+# we can add an element in the list using append() method
+thislist = ["Javascript", "Python", "Java"]
+thislist.append("C++")
+print(thislist)
+# result will be ['Javascript', 'Python', 'Java', 'C++']
+# the element will add at end of the list
+
+# We can add element in list at particular index using insert() method
+thislist = ["Javascript", "Python", "Java"]
+thislist.insert(1,"C++")
+print(thislist)
+# the result will be ['Javascript', 'C++', 'Python', 'Java']
