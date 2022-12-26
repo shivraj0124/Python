69eba9fbd25f25c573ee93c3fd4aec696a898fd1
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-26 22:14:42 +0530

    Change_List

---
+++ Basics/List/F3_ChangeList.py
@@ -0,0 +1,10 @@
+# we can change the values present in the list using there index
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+# we can change the values in particular range of index
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["Grape", "watermelon"]
+print(thislist)
