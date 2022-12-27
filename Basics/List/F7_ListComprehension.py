5dccbad7d45ac3519c49fb3f8f0d169d54daf5e5
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2022-12-27 20:57:45 +0530

    Comprehension

---
+++ Basics/List/F7_ListComprehension.py
@@ -0,0 +1,16 @@
+# List comprehension offers shorter syntax when we have to create new list based on existing list
+thislist = ["Python","Javascript","Java","C","C++","Php"]
+newList=[]
+
+for x in thislist:
+    if 'a' in x:
+        newList.append(x)
+print(newList)
+
+
+# List comprehension in one line
+thislist = ["Python", "Javascript", "Java", "C", "C++", "Php"]
+newList = [x for x in thislist if 'a' in x]
+print(newList)
+
+print(thislist.index())
