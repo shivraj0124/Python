cd10d1f20d3981c84ccdc2bcb6fc1e2ab48d8050
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2023-01-04 21:57:04 +0530

    IfElse

--- Basics/ConditionalStatement/F7_IfElse.py
+++ Basics/ConditionalStatement/F7_IfElse.py
@@ -2,11 +2,12 @@ a = int(input("Enter a number: "))
 print("Entered number is:", a)
 
 # Conditional operators 
-# >, <, >=, <=, ==, !=
-# print(a>18)
-# print(a<=18)
-# print(a==18)
-# print(a!=18)
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
 
 # if condition will true then if block will execute, Output will 'Yes' 
 if(a > 20):
